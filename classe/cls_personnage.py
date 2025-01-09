4ee86d17d8b6773a3471d111f0f08637f01aa400
LLS-lopos <luderickls@gmail.com>
2025-01-09 12:50:47 -0400

    utilisation d'un dictionnaire

--- classe/cls_personnage.py
+++ classe/cls_personnage.py
@@ -3,39 +3,45 @@ from random import randint
 
 class Personnage:
     def __init__(self, NOM, PV, ATT):
-        self.nom = NOM
-        self.pv = PV
-        self.att = ATT
-        self.statistique = {
+        self.stats = {
+            "nom": NOM,
             "pv": PV,
             "att": ATT,
         }
 
     def attaquer(self, cible):
-        cible.pv -= self.att
-        ic(cible.nom, cible.pv)
-        if cible.pv <= 0:
+        cible.stats["pv"] -= self.stats["att"]
+        ic(cible.stats["nom"], cible.stats["pv"])
+        if cible.stats["pv"] <= 0:
             cible.mort()
     
     def niveau_sup(self):
-        for cle in self.statistique:
-            ic(cle, self.statistique[cle])
-            old = self.statistique.get(cle)
-            self.statistique[cle] += randint(0, 5)
-            diff = self.statistique[cle] - old
-            ic(cle, self.statistique[cle])
+        for cle in self.stats:
+            ic(cle, self.stats[cle])
+            old = self.stats.get(cle)
+            self.stats[cle] += randint(0, 5)
+            diff = self.stats[cle] - old
+            ic(cle, self.stats[cle])
             ic(diff)
 
     def mort(self):
-        if self.pv <= 0:
-            self.pv = 0
-            ic(self.nom + " est mort au combat")
+        if self.stats["pv"] <= 0:
+            self.stats["pv"] = 0
+            ic(self.stats["nom"] + " est mort au combat")
+    
+    def __str__(self):
+        for cle in self.stats:
+            print(f"{cle}: {self.stats[cle]}")
 
 if __name__ == "__main__":
     Dai = Personnage("Daï", 12, 5)
     Croco = Personnage("Dino", 10, 2)
+    Dai.__str__()
+    Croco.__str__()
     Dai.attaquer(Croco)
     Croco.attaquer(Dai)
     Croco.attaquer(Dai)
     Croco.attaquer(Dai)
     Dai.attaquer(Croco)
+    Dai.__str__()
+    Croco.__str__()
