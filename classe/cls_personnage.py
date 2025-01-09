5bf977840e4516bd622cbc13b0de7b59a3556e53
LLS-lopos <luderickls@gmail.com>
2025-01-09 15:25:12 -0400

    ajoute fonction attaque critique

--- classe/cls_personnage.py
+++ classe/cls_personnage.py
@@ -1,47 +1,88 @@
-from icecream import ic
 from random import randint
+from fonc_math import arrondire
 
 class Personnage:
-    def __init__(self, NOM, PV, ATT):
+    def __init__(self, NOM, PV, ATT, DEF, VIT):
         self.stats = {
             "nom": NOM,
             "pv": PV,
             "att": ATT,
+            "def": DEF,
+            "vit": VIT,
+            "exp": 0,
+            "exp_max": 100,
+            "att_critique": 0.2,
         }
 
     def attaquer(self, cible):
-        cible.stats["pv"] -= self.stats["att"]
-        ic(cible.stats["nom"], cible.stats["pv"])
-        if cible.stats["pv"] <= 0:
-            cible.mort()
+        att_init = self.stats["att"]
+        self.att_critique()
+        old = cible.stats["pv"]
+        attaque = self.stats["att"] - cible.stats["def"]
+        if attaque > 0:
+            cible.stats["pv"] -= attaque
+            print(f"{self.stats["nom"]} attaque {cible.stats["nom"]} et lui inflige {old - cible.stats["pv"]} points de dégâts")
+            if self.stats["att"] != att_init: print(f"{self.stats["nom"]} porter un coup critique à {cible.stats["nom"]}")
+        else: print("l'attaque est sans effet")
+        if cible.stats["pv"] <= 0: cible.mort()
+        else: print(f"{cible.stats["nom"]} a {cible.stats["pv"]} points de vie")
+        self.stats["att"] = att_init
     
     def niveau_sup(self):
         for cle in self.stats:
-            ic(cle, self.stats[cle])
-            old = self.stats.get(cle)
-            self.stats[cle] += randint(0, 5)
-            diff = self.stats[cle] - old
-            ic(cle, self.stats[cle])
-            ic(diff)
+            if cle == "nom":
+                print(f"{self.stats.get(cle)}")
+            else:
+                old = self.stats.get(cle)
+                self.stats[cle] += randint(0, 5)
+                print(f"{cle.capitalize()}: {old} -> {self.stats[cle]} (+{self.stats[cle] - old})")
 
     def mort(self):
         if self.stats["pv"] <= 0:
             self.stats["pv"] = 0
-            ic(self.stats["nom"] + " est mort au combat")
+            print(self.stats["nom"] + " n'a plus de points de vie et est mort au combat")
     
     def __str__(self):
         for cle in self.stats:
             print(f"{cle}: {self.stats[cle]}")
 
+    def att_critique(self):
+        a, b, c, d = randint(0, 1), randint(0, 1), randint(0, 1), randint(0, 1)
+        if a == b == c == d:
+            self.stats["att"] = self.stats["att"] + arrondire(self.stats["att"], self.stats["att_critique"])
+            return self.stats["att"]
+
+class Guerrier(Personnage):
+    def __init__(self, NOM="", PV=20, ATT=7, DEF=8, VIT=3):
+        Personnage.__init__(self, NOM, PV, ATT, DEF, VIT)
+        self.stats = {
+            "nom": NOM,
+            "pv": PV,
+            "att": ATT,
+            "def": DEF,
+            "vit": VIT,
+            "exp": 0,
+            "exp_max": 100,
+            "att_critique": 1.2,
+        }
+    
+    def niveau_sup(self):
+        for cle in self.stats:
+            if cle == "nom":
+                print(f"{self.stats.get(cle)}")
+            elif cle == "pv":
+                old = self.stats.get(cle)
+                self.stats[cle] += randint(0, 5)
+                print(f"{cle.capitalize()}: {old} -> {self.stats[cle]} (+{self.stats[cle] - old})")
+            else:
+                old = self.stats.get(cle)
+                self.stats[cle] += randint(2, 3)
+                print(f"{cle.capitalize()}: {old} -> {self.stats[cle]} (+{self.stats[cle] - old})")
+            
+
 if __name__ == "__main__":
-    Dai = Personnage("Daï", 12, 5)
-    Croco = Personnage("Dino", 10, 2)
-    Dai.__str__()
-    Croco.__str__()
-    Dai.attaquer(Croco)
-    Croco.attaquer(Dai)
-    Croco.attaquer(Dai)
-    Croco.attaquer(Dai)
-    Dai.attaquer(Croco)
-    Dai.__str__()
-    Croco.__str__()
+    Dai = Personnage("Daï", 12, 5, 6, 8)
+    Croco = Personnage("Dino", 20, 2, 4, 12)
+    Polo = Guerrier("Polo")
+    for i in range(4):
+        Polo.attaquer(Croco)
